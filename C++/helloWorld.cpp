59696605dcf724b65a13dfc77bc39c7d797e26a8
Eddie <mahamaek@gmail.com>
2020-05-18 23:13:13 -0400

    dynamic helloWorld

--- C++/helloWorld.cpp
+++ C++/helloWorld.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main(void)
 {
-    cout << "Hello, World" << endl;
+    string name;
+    cout << "What is your name: " << endl;
+    cin >> name;
+    cout << "Hello " << name << endl;
     return 0;
 }
