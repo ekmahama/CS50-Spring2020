5ee7910c493c9b86d09f5a286aa15d18201bc111
Eddie <mahamaek@gmail.com>
2020-05-20 14:26:54 -0400

    first commit

---
+++ C/Lecture 1/mario.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+void printHashes(int n);
+/*
+My implementation of supermario
+*/
+
+int main(void)
+{ 
+    int n;
+    do
+    {
+        n = get_int("height: ");
+    }
+    while (n < 1 || n > 8);
+
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int m=1; m < n-i; m++)
+        {
+            printf(" ");
+        }   
+        printHashes(i);
+        printf("  "); // print double space
+        printHashes(i);
+        printf("\n"); // Print newline
+    }
+
+}
+
+void printHashes(int n)
+{
+    for (int j =0; j <= n; j++)
+    {
+        printf("#");
+    }
+}
+
+
+
