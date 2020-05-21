08fc3f09d8c0487b5a64adb391bc1527ce213ab3
Eddie <mahamaek@gmail.com>
2020-05-21 00:25:22 -0400

    first commit

--- C/Lecture 2/array0.c
+++ C/Lecture 2/array0.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
 
+int main()
+{
+    // Chars can store ints
+    unsigned char numbers[] = {2, 3, 4, 5};
+
+    int scores[4] = {95, 97, 96, 98};
+
+    char *names[] = {"Edward", "Mavis", "Kofi", "Lark"};
+
+    // A string is an array of chars
+    string s = get_string("Input: ");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+    printf("%s\n", s);
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     printf("i = %i\n", numbers[i]);
+    //     printf("score %i : %i\n", i, scores[i]);
+    //     printf("Name %i : %s\n", i, names[i]);
+    // }
+
+    return 0;
+}
