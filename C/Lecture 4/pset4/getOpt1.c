ca54410f496caf05d0fec7360e88bc06b89ceed6
Eddie <mahamaek@gmail.com>
2020-06-05 22:25:02 -0400

    Using optind

---
+++ C/Lecture 4/pset4/getOpt1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+void print_usage();
+void print_farenheit(float c);
+void print_centigrate(float f);
+
+int main(int argc, char *argv[])
+{
+    int options;
+    int cflag = 0;
+    int fflag = 0;
+
+    // Colon means argument after the option is mandatory
+    while ((options = getopt(argc, argv, "cf")) != -1)
+    {
+        switch (options)
+        {
+        case 'c':
+            if (cflag)
+            {
+                print_usage();
+            }
+            else
+            {
+                cflag++;
+                fflag++;
+            }
+            print_centigrate(atof(argv[optind]));
+            printf("%s\n", argv[optind]);
+            break;
+
+        case 'f':
+            if (fflag)
+            {
+                print_usage();
+            }
+            else
+            {
+                fflag++;
+                cflag++;
+            }
+            print_farenheit(atof(argv[optind]));
+            printf("%s\n", argv[optind]);
+            break;
+
+        default:
+            print_usage();
+            break;
+        }
+    }
+    return 0;
+}
+
+void print_usage()
+{
+    printf("Usage: ./getOpt -c n | ./ getOpt -f n ");
+    exit(1);
+}
+
+void print_farenheit(float c)
+{
+    float f = ((c * 9) / 5) + 32;
+    printf("Farenheit Equivalent: %.2f\n", f);
+}
+
+void print_centigrate(float f)
+{
+    float c = ((f - 32) * 5) / 9;
+    printf("Centigrae Equivalent: %.2f\n", c);
+}
